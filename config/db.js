1fa3458e500aef75c3095ae4d2297b513ce7fd7f
rakutenjongseok <whdtjr2792@gmail.com>
2019-10-19 17:31:19 +0900

    config

--- config/db.js
+++ config/db.js
@@ -4,11 +4,10 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
+    useFindAndModify: true,
     useUnifiedTopology: true,
   });
-
-  console.log(`MongoDB Connected : ${conn.connection.host}`.cyan.underline.bold);
+  console.log(`MongoDb Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
