07258e93674959fe76f52898aaa9dc8b7ee58621
rakutenjongseok <whdtjr2792@gmail.com>
2019-10-21 23:35:52 +0900

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 
 // @desc   Register user
-// @route  GET /api/v1/auth/register
+// @route  POST /api/v1/auth/register
 // @access Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -15,6 +15,40 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+// @desc   Login user
+// @route  POST /api/v1/auth/login
+// @access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse('please provide an email and password', 401));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Password is not correct!', 401));
+  }
 
   // Create token
   const token = user.getSignedJwtToken();
