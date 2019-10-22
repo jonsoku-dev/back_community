29848a0cad76e320e44a3b168d0dabc960bba3df
rakutenjongseok <whdtjr2792@gmail.com>
2019-10-22 12:49:47 +0900

    xss protection & security headers

--- README.md
+++ README.md
@@ -20,8 +20,30 @@ https://mailtrap.io
 <br>
 https://nodemailer.com/about/
 
+# API SECURITY
+
 ## mongo-sanitize
 
 https://github.com/vkarpov15/mongo-sanitize#readme
 <br>
 https://github.com/fiznool/express-mongo-sanitize
+
+## helmet
+
+여러 공격으로부터 보호해줌 아래사이트에서 확인
+
+https://helmetjs.github.io/
+
+```
+$ yarn add helmet
+```
+
+## xss-clean
+
+input 태그에 자바스크립트 입력하는 거방지
+
+https://github.com/jsonmaur/xss-clean
+
+```
+$ yarn add xss-clean
+```
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -42,6 +44,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
